3a9c9a4347220dd96ad7294fe3b943e23d1690c2
sarahtalkhabi <sara.talkha@gmail.com>
2023-06-08 14:40:17 +0000

    seperate auth and profile

--- account/serializers.py
+++ account/serializers.py
@@ -9,7 +9,7 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id',"username",'first_name','last_name','email']
+        fields = ['id','username','email']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -19,16 +19,12 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+
 
     class Meta:
         model = User
         fields = ('username', 'password',
-                  'email', 'first_name', 'last_name')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
+                  'email')
 
     # def validate(self, attrs):
     #     if attrs['password'] != attrs['password2']:
@@ -40,9 +36,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
+           )
         user.set_password(validated_data['password'])
         user.save()
         return user
